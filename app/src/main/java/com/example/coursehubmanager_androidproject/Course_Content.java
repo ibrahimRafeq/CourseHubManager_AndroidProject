b85a66fd458e580d2ca96d15610283e0bdba6666
IBRAHIM AL HADY <92710703+IbeahimALHady@users.noreply.github.com>
2025-01-31 10:00:14 +0200

    updateNew

--- app/src/main/java/com/example/coursehubmanager_androidproject/Course_Content.java
+++ app/src/main/java/com/example/coursehubmanager_androidproject/Course_Content.java
@@ -6,12 +6,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
-
 import com.example.coursehubmanager_androidproject.databinding.ActivityCourseContentBinding;
-
 import java.util.ArrayList;
 import java.util.List;
 
