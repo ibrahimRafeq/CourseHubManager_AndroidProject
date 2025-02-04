5cc0988e2b36deeea59cefdf235a524daa676879
IBRAHIM AL HADY <92710703+IbeahimALHady@users.noreply.github.com>
2025-02-04 17:19:00 +0200

    editNEW

--- app/src/main/java/com/example/coursehubmanager_androidproject/CourseDao.java
+++ app/src/main/java/com/example/coursehubmanager_androidproject/CourseDao.java
@@ -22,24 +22,6 @@ public interface CourseDao {
     @Query("select * from Course where courseId =:id order by courseName asc")
     List<Course> getAllCourseDetails(long id);
 
-    @Query("SELECT * FROM Course WHERE isJoined = 1 and isCompleted = 0")
-    List<Course> getJoinedCourses();
-
-    @Query("UPDATE Course SET isJoined = 1 WHERE courseId =:courseId")
-    void joinCourse(long courseId);
-
-    @Query("SELECT * FROM Course WHERE isCompleted = 1")
-    List<Course> getCompletedCourses();
-
-    @Query("UPDATE Course SET isCompleted = 1 WHERE courseId =:courseId")
-    void updateCourseCompletion(long courseId);
-
-    @Query("SELECT * FROM Course WHERE isBookMark = 1")
-    List<Course> getBookMarkCourses();
-
-    @Query("UPDATE Course SET isBookMark = 1 WHERE courseId =:courseId")
-    void updateBookMark(long courseId);
-
     @Update
     int updateCourse(Course course);
 
